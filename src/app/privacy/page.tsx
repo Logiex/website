ea5faab2b08e593aae595b1f6249815518e829b3
Buchimi <stormzzy900@gmail.com>
2023-12-19 22:40:00 -0600

    update privacy policy and tos

--- src/app/privacy/page.tsx
+++ src/app/privacy/page.tsx
@@ -2,23 +2,32 @@
 
 import React from "react";
 
+
+// PrivacyPolicy.js
+
 const PrivacyPolicy = () => {
   return (
-    <div>
-      <div>
+    <div className="min-h-screen flex items-center justify-center bg-gray-100 text-black">
+      <div className="max-w-md w-full bg-white p-8 rounded shadow-lg">
         <h2 className="text-2xl font-semibold mb-4">Privacy Policy</h2>
+
+        {/* Last Updated */}
+        <p className="text-sm text-gray-500 mb-4">Last updated: 12/18/23</p>
+
+        {/* Introduction */}
         <p>
-          Privacy Policy for Rondevu Mobile App Last updated: 12/18/23 Rondevu (
-          &quot; Rondevu &quot; , &quot; us &quot; , &quot; we &quot; , or
-          &quot; our &quot; ) operates the Rondevu mobile application (the
-          &quot; App &quot; ). This page informs you of our policies regarding
-          the collection, use, and disclosure of personal data when you use our
-          App and the choices you have associated with that data. We use your
-          data to provide and improve the App, connecting users with activities
-          they may be interested in within their local communities. By using the
-          App, you agree to the collection and use of information in accordance
-          with this policy.
+          Rondevu (&quot;Rondevu,&quot; &quot;us&quot; &quot;we,&quot; or
+          &quot;our&quot;) operates the Rondevu mobile application (the
+          &quot;App&quot;). This page informs you of our policies regarding the
+          collection, use, and disclosure of personal data when you use our App
+          and the choices you have associated with that data. We use your data
+          to provide and improve the App, connecting users with activities they
+          may be interested in within their local communities. By using the App,
+          you agree to the collection and use of information in accordance with
+          this policy.
         </p>
+
+        {/* Information Collection and Use */}
         <section className="mb-6">
           <h3 className="text-xl font-semibold mb-2">
             Information Collection and Use
@@ -27,68 +36,80 @@ const PrivacyPolicy = () => {
             We collect several types of information for various purposes to
             provide and enhance our service to you.
           </p>
-          {/* Add your content here */}
         </section>
+
         {/* Types of Data Collected */}
         <section className="mb-6">
           <h3 className="text-xl font-semibold mb-2">
             Types of Data Collected
           </h3>
-          <h3 className="text-l font-semibold mb-2">Personal Data </h3>
+          <p>
+            <strong>Personal Data</strong>
+          </p>
           <p>
             While using our App, we may ask you to provide us with certain
-            personally identifiable information to enhance your experience (
-            &quot; Personal Data &quot; ). Personally identifiable information
-            may include, but is not limited to:
+            personally identifiable information to enhance your experience
+            (&quot;Personal Data&quot;). Personally identifiable information may
+            include, but is not limited to:
           </p>
-          <ul>
+          <ul className="list-disc pl-6">
             <li>Email address</li>
             <li>Location data</li>
             <li>Interests and preferences</li>
             <li>User-generated content</li>
           </ul>
-          <h3 className="text-l font-semibold mb-2">Usage Data</h3>
-          We may also collect information on how the App is accessed and used
-          (&quot; Usage Data&quot; ). This Usage Data may include information
-          such as your device &apos; s Internet Protocol address (e.g., IP
-          address), device type, operating system, app version, the pages of our
-          App that you visit, the time and date of your visit, and other
-          diagnostic data.
+          <p>
+            <strong>Usage Data</strong>
+          </p>
+          <p>
+            We may also collect information on how the App is accessed and used
+            (&quot;Usage Data&quot;). This Usage Data may include information
+            such as your device&apos;s Internet Protocol address (e.g., IP
+            address), device type, operating system, app version, the pages of
+            our App that you visit, the time and date of your visit, and other
+            diagnostic data.
+          </p>
         </section>
+
         {/* Use of Data */}
         <section className="mb-6">
           <h3 className="text-xl font-semibold mb-2">Use of Data</h3>
-          {/* Add your content here */}
-          <div>
-            Rondevu uses the collected data for various purposes:
-            <ul>
-              <li>To provide and personalize the App experience</li>
-              <li>
-                To match users with activities based on their interests and
-                location
-              </li>
-              <li>
-                To notify users about events and updates related to their
-                interests and local communities
-              </li>
-              <li>
-                To facilitate communication between users with shared interests
-              </li>
-              <li>
-                To gather insights for analysis and improvement of the App
-              </li>
-              <li>To monitor the usage of the App</li>
-              <li>To detect, prevent, and address technical issues</li>
-            </ul>
-          </div>
+          <p>Rondevu uses the collected data for various purposes:</p>
+          <ul className="list-disc pl-6">
+            <li>To provide and personalize the App experience</li>
+            <li>
+              To match users with activities based on their interests and
+              location
+            </li>
+            <li>
+              To notify users about events and updates related to their
+              interests and local communities
+            </li>
+            <li>
+              To facilitate communication between users with shared interests
+            </li>
+            <li>To gather insights for analysis and improvement of the App</li>
+            <li>To monitor the usage of the App</li>
+            <li>To detect, prevent, and address technical issues</li>
+          </ul>
         </section>
 
         {/* Location Data */}
+
         <section className="mb-6">
           <h3 className="text-xl font-semibold mb-2">Location Data</h3>
           <p>
-            <b>Third-party Partners</b>
+            Rendovu utilizes location data to match users with activities
+            happening in their local communities. Users have the option to
+            control location services through their device settings.
           </p>
+        </section>
+
+        {/* Disclosure of Data */}
+        <section className="mb-6">
+          <h3 className="text-xl font-semibold mb-2">Disclosure of Data</h3>
+          {/* Add your content here */}
+          <h5>Third party partners</h5>
           <p>
             Rondevu may collaborate with third-party partners to enhance the App
             experience. These partners may have access to your Personal Data
@@ -96,46 +117,52 @@ const PrivacyPolicy = () => {
             disclose or use it for any other purpose.
           </p>
         </section>
-        {/* Disclosure of Data */}
-        <section className="mb-6">
-          <h3 className="text-xl font-semibold mb-2">Disclosure of Data</h3>
-          {/* Add your content here */}
-        </section>
+
         {/* Security of Data */}
         <section className="mb-6">
           <h3 className="text-xl font-semibold mb-2">Security of Data</h3>
-          {/* Add your content here */}
-          The security of your data is important to us. We strive to use
-          commercially acceptable means to protect your Personal Data, but no
-          method of transmission over the Internet or method of electronic
-          storage is 100% secure. We cannot guarantee its absolute security.
+          <p>
+            The security of your data is important to us. We strive to use
+            commercially acceptable means to protect your Personal Data, but no
+            method of transmission over the Internet or method of electronic
+            storage is 100% secure. We cannot guarantee its absolute security.
+          </p>
         </section>
+
         {/* Your Data Protection Rights */}
         <section className="mb-6">
           <h3 className="text-xl font-semibold mb-2">
             Your Data Protection Rights
           </h3>
-          Rondevu aims to take reasonable steps to allow you to correct, amend,
-          delete, or limit the use of your Personal Data. Users can manage their
-          privacy settings within the App or by Email.
+          <p>
+            Rondevu aims to take reasonable steps to allow you to correct,
+            amend, delete, or limit the use of your Personal Data. Users can
+            manage their privacy settings within the App or by Email.
+          </p>
         </section>
+
         {/* Changes to This Privacy Policy */}
         <section className="mb-6">
           <h3 className="text-xl font-semibold mb-2">
             Changes to This Privacy Policy
           </h3>
-          We may update our Privacy Policy from time to time. We will notify you
-          of any changes by posting the new Privacy Policy on this page. You are
-          advised to review this Privacy Policy periodically for any changes.
-          Changes to this Privacy Policy are effective when they are posted on
-          this page.
+          <p>
+            We may update our Privacy Policy from time to time. We will notify
+            you of any changes by posting the new Privacy Policy on this page.
+            You are advised to review this Privacy Policy periodically for any
+            changes. Changes to this Privacy Policy are effective when they are
+            posted on this page.
+          </p>
         </section>
+
         {/* Contact Us */}
         <section>
           <h3 className="text-xl font-semibold mb-2">Contact Us</h3>
-          If you have any questions about this Privacy Policy, please contact
-          us: - By email: [Your email address] - Through the App: [Include
-          relevant in-app contact information]
+          <p>
+            If you have any questions about this Privacy Policy, please contact
+            us: - By email: [Your email address] - Through the App: [Include
+            relevant in-app contact information]
+          </p>
         </section>
       </div>
     </div>
--- src/app/tos/page.tsx
+++ src/app/tos/page.tsx
@@ -1,6 +1,156 @@
-const TOS = () => {
-  return <div>TOS</div>;
+// TermsOfService.js
+
+import React from "react";
+
+const TermsOfService = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100 text-black">
+      <div className="max-w-md w-full bg-white p-8 rounded shadow-lg">
+        <h2 className="text-2xl font-semibold mb-4">Terms of Service</h2>
+        {/* Last Updated */}
+        <p className="text-sm text-gray-500 mb-4">Last updated: 12/18/23</p>
+
+        {/* Introduction */}
+        <p className="mb-6 text-black">
+          This document outlines the terms and conditions (&quot;Terms&quot;)
+          governing the use of the Rendovu mobile application (the &quot; App
+          &quot; ) provided by Rondevu ( &quot; us, &quot; &quot; we, &quot; or
+          &quot; our &quot; ).
+        </p>
+
+        {/* Acceptance of Terms */}
+        <section className="mb-6 text-black">
+          <h3 className="text-xl font-semibold mb-2">1. Acceptance of Terms</h3>
+          <p>
+            By using the App, you acknowledge that you have read, understood,
+            and agree to be bound by these Terms. We reserve the right to update
+            or modify these Terms at any time without prior notice. It is your
+            responsibility to review these Terms periodically for changes.
+            Continued use of the App after the posting of changes constitutes
+            acceptance of those changes.
+          </p>
+        </section>
+
+        {/* User Accounts and Registration */}
+        <section className="mb-6 text-black">
+          <h3 className="text-xl font-semibold mb-2">
+            2. User Accounts and Registration
+          </h3>
+          <p>
+            To access certain features of the App, you may be required to
+            register for an account. You agree to provide accurate, current, and
+            complete information during the registration process and to update
+            such information to keep it accurate, current, and complete.
+          </p>
+          <p>
+            You are responsible for maintaining the confidentiality of your
+            account and password and for restricting access to your device. You
+            agree to accept responsibility for all activities that occur under
+            your account or password.
+          </p>
+        </section>
+
+        {/* User Conduct */}
+        <section className="mb-6 text-black">
+          <h3 className="text-xl font-semibold mb-2">3. User Conduct</h3>
+          <p>
+            You agree to use the App in accordance with all applicable laws and
+            regulations. You further agree not to:
+          </p>
+          <ul className="list-disc pl-6">
+            <li>Use the App for any illegal or unauthorized purpose.</li>
+            <li>
+              Interfere with or disrupt the operation of the App or the servers
+              or networks used to make the App available.
+            </li>
+            <li>
+              Impersonate any person or entity or provide false information.
+            </li>
+          </ul>
+        </section>
+
+        {/* Privacy Policy */}
+        <section className="mb-6 text-black">
+          <h3 className="text-xl font-semibold mb-2">4. Privacy Policy</h3>
+          <p>
+            Your use of the App is also governed by our Privacy Policy, which is
+            available{" "}
+            <a href="/privacy-policy" className="text-blue-500">
+              here
+            </a>
+            . By using the App, you agree to the terms of the Privacy Policy.
+          </p>
+        </section>
+
+        {/* Intellectual Property */}
+        <section className="mb-6 text-black">
+          <h3 className="text-xl font-semibold mb-2">
+            5. Intellectual Property
+          </h3>
+          <p>
+            The App and its original content, features, and functionality are
+            owned by Rondevu and are protected by international copyright,
+            trademark, patent, trade secret, and other intellectual property or
+            proprietary rights laws.
+          </p>
+        </section>
+
+        {/* Termination */}
+        <section className="mb-6 text-black">
+          <h3 className="text-xl font-semibold mb-2">6. Termination</h3>
+          <p>
+            We reserve the right to terminate or suspend your account and access
+            to the App at our sole discretion, without prior notice or
+            liability, for any reason whatsoever, including without limitation
+            if you breach these Terms.
+          </p>
+        </section>
+
+        {/* Disclaimer of Warranties */}
+        <section className="mb-6 text-black">
+          <h3 className="text-xl font-semibold mb-2">
+            7. Disclaimer of Warranties
+          </h3>
+          <p>
+            The App is provided &quot; as is &quot; and &quot; as available
+            &quot; without any warranties of any kind, whether express or
+            implied.
+          </p>
+        </section>
+
+        {/* Limitation of Liability */}
+        <section className="mb-6 text-black">
+          <h3 className="text-xl font-semibold mb-2">
+            8. Limitation of Liability
+          </h3>
+          <p>
+            To the fullest extent permitted by applicable law, Rondevu shall not
+            be liable for any indirect, incidental, special, consequential, or
+            punitive damages, or any loss of profits or revenues.
+          </p>
+        </section>
+
+        {/* Governing Law */}
+        <section className="mb-6 text-black">
+          <h3 className="text-xl font-semibold mb-2">9. Governing Law</h3>
+          <p>
+            These Terms are governed by and construed in accordance with the
+            laws of the United States, without regard to its conflict of law
+            principles.
+          </p>
+        </section>
+
+        {/* Contact Us */}
+        <section className="text-black">
+          <h3 className="text-xl font-semibold mb-2 ">10. Contact Us</h3>
+          <p>
+            If you have any questions about these Terms, please contact us at
+            support@rondevu.app.
+          </p>
+        </section>
+      </div>
+    </div>
+  );
 };
 
-
-export default TOS
+export default TermsOfService;
