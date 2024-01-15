5d523d4071231e28a9be42b932111bd06916b743
jduhking <jamesbodebiyi@gmail.com>
2024-01-15 15:20:46 -0600

    Deep Linking work should work

--- src/app/testing/page.tsx
+++ src/app/testing/page.tsx
@@ -1,7 +1,7 @@
 export default function Hello() {
   return (
     <div>
-      YOooo <a href="https://website-mu-five-93.vercel.app/rondevu/65a569f5d07a3cd2cf17a190">Click heres</a>{" "}
+      YOooo
     </div>
   );
 }
