41f24c5bd4a46e958388de1577cf70331b1d7771
jduhking <jamesbodebiyi@gmail.com>
2024-01-15 15:02:19 -0600

    Rondevu test link

--- src/app/testing/page.tsx
+++ src/app/testing/page.tsx
@@ -1,7 +1,7 @@
 export default function Hello() {
   return (
     <div>
-      YOooo <a href="https://rondevu.app">Click heres</a>{" "}
+      YOooo <a href="https://rondevu.app/rondevu/65a569f5d07a3cd2cf17a190">Click heres</a>{" "}
     </div>
   );
 }
