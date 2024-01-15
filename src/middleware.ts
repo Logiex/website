598b5c63918c6a29aedaecb6a34ca07ad3120454
Buchimi <stormzzy900@gmail.com>
2024-01-15 00:21:50 -0600

    update 202

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,25 @@
 import { authMiddleware } from "@clerk/nextjs";
- 
+
 export default authMiddleware({
-  ignoredRoutes:["((?!^/polls/create).*)"],
-  
+  ignoredRoutes: ["((?!^/polls/create).*)"],
 });
- 
+
+import type { NextRequest } from "next/server";
+import { NextResponse, userAgent } from "next/server";
+
+export function middleware(req: NextRequest) {
+  const { ua } = userAgent(req);
+  if (req.nextUrl.pathname.startsWith("/download")) {
+    if (/iP(hone|ad|od)/.test(ua)) {
+      return NextResponse.redirect("https://google.com");
+    } else if (/Android/.test(ua)) {
+      return NextResponse.redirect("https://youtube.com");
+    } else {
+      return NextResponse.redirect("https://twitter.com");
+    }
+  }
+}
+
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)", "/download"],
 };
