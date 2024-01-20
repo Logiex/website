7c452c536e0bf5231037a41728f024b81bd29e0e
Buchi <stormzzy900@gmail.com>
2024-01-20 03:38:46 -0600

    ask user for username before viewing results

--- src/app/testing/[slug]/page.tsx
+++ src/app/testing/[slug]/page.tsx
@@ -1,7 +1,8 @@
 "use client";
 import PollResult from "@/pages/PollResults";
-import { gql, useQuery } from "@apollo/client";
-import { useRef } from "react";
+import { gql, useMutation, useQuery } from "@apollo/client";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { SubmitHandler, useForm } from "react-hook-form";
 
 const GETPOLLQUERY = gql`
   query MyQuery($ID: PydanticObjectId!) {
@@ -12,9 +13,21 @@ const GETPOLLQUERY = gql`
         votes
       }
     }
+    pollUser {
+      displayName
+    }
   }
 `;
 
+const UPDATEDISPLAYNAMEMUTATION = gql`
+  mutation MyMutation($Name: String!) {
+    setPollUserName(name: $Name) {
+      _id
+      displayName
+      username
+    }
+  }
+`;
 type getPoll = {
   options: string[];
   results: { option: string; votes: number }[];
@@ -50,21 +63,98 @@ const parseResults = (
   });
   return { graph: res, total: total };
 };
+const parseDisplayName = (data: any) => {
+  if (!data) {
+    return undefined;
+  }
+  return data.pollUser.displayName;
+};
 
+const SubmitDisplayName = ({
+  onUsernameSubmitted,
+}: {
+  onUsernameSubmitted: () => void;
+}) => {
+  const [callback] = useMutation(UPDATEDISPLAYNAMEMUTATION, {
+    refetchQueries: [GETPOLLQUERY],
+  });
+
+  interface IFormInput {
+    name: String;
+  }
+  const { register, handleSubmit } = useForm<IFormInput>();
+  const onSubmit: SubmitHandler<IFormInput> = (data) => {
+    callback({
+      variables: {
+        Name: data.name,
+      },
+    });
+    onUsernameSubmitted();
+  };
+
+  return (
+    <div className=" flex h-screen justify-center items-center">
+      <Card className="bg-white min-h-[50%] min-w-[75%] lg:min-w-[50%]  p-4 text-black">
+        <CardHeader>
+          <CardTitle>You need a display name to view this poll</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="flex flex-col space-y-1.5">
+              <label htmlFor="name">Name</label>
+              <input
+                id="name"
+                placeholder="Display name"
+                {...register("name")}
+                className="bg-neutral-100	"
+              />
+            </div>
+            <div className="flex justify-end w-full">
+              <input type="submit" className="py-4 hover:cursor-pointer " />
+            </div>
+          </form>
+
+        </CardContent>
+
+      </Card>
+
+    </div>
+  );
+};
 export default function Testing({ params }: { params: { slug: string } }) {
   const id = params.slug;
-  const { data, loading } = useQuery(GETPOLLQUERY, {
+  const { data, loading, refetch, client } = useQuery(GETPOLLQUERY, {
     variables: {
       ID: id,
     },
   });
   const result = parseResults(data);
+  const displayName = parseDisplayName(data);
 
   return (
-    <PollResult
-      options={result?.graph}
-      votes={result?.total}
-      loading={loading}
-    />
+    <div>
+      {loading && (
+        <PollResult
+          options={result?.graph}
+          votes={result?.total}
+          loading={loading}
+        />
+      )}
+      {!loading && data && displayName ? (
+        <PollResult
+          options={result?.graph}
+          votes={result?.total}
+          loading={loading}
+        />
+      ) : (
+        <SubmitDisplayName
+          onUsernameSubmitted={() => {
+            // refetch({ ID: id });
+
+            console.log("yess");
+          }}
+        />
+      )}
+    </div>
   );
 }
