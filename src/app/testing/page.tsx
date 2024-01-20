516401d27d58e627fa53a8bad97d67a6975a1f54
Buchimi <stormzzy900@gmail.com>
2024-01-19 19:05:07 -0600

    abstract the rondevu response page

--- src/app/testing/page.tsx
+++ src/app/testing/page.tsx
@@ -34,39 +34,76 @@ const PilledButton = ({
   );
 };
 
-export default function Hello() {
+export default function RondevuResults({
+  votes,
+  options,
+  title,
+  friends,
+}: {
+  votes?: number;
+  options: { name: string; results: number }[];
+  title?: number;
+  friends?: { name: string; choice: string }[];
+}) {
   return (
     <div className={`pt-8 flex flex-col items-center pl-2 ${dongle.className}`}>
       <div className=" w-[70%] bg-white relative bg-black rounded-3xl">
         <div className="relative bottom-2 right-2 w-full rounded-3xl h-full bg-white p-4">
-          <div className="text-7xl flex justify-center py-16 relative">
-            This is a title
-            <div className="absolute text-xl -left-24 bg-white rounded-full outline p-2 outline-2 outline-black">
-              24 votes
-            </div>
+          <div className="absolute right-[5%] top-[5%]  hover:cursor-pointer text-3xl outline outline-2 z-10 p-1 px-2">
+            Share
+          </div>
+          <div className="absolute text-xl -left-8 bg-white rounded-full outline p-2 outline-2 outline-black">
+            {votes ?? 24} votes
+          </div>
+          <div className="text-5xl flex justify-center py-16 relative ">
+            {title ?? "Whataburger or Raising canes"}
           </div>
           <div className="px-8 items-center text-2xl">
-            <PilledButton percentage={90}>Raising Canes</PilledButton>
-            <PilledButton percentage={10}>Whataburger</PilledButton>
+            {options ? (
+              options.map((options, index) => {
+                return (
+                  <PilledButton key={index} percentage={options.results}>
+                    {options.name}
+                  </PilledButton>
+                );
+              })
+            ) : (
+              <div>
+                <PilledButton percentage={90}>Raising Canes</PilledButton>
+                <PilledButton percentage={10}>Whataburger</PilledButton>
+              </div>
+            )}
             <div className="flex flex-col sm:flex-row">
               <CreateRondevuButton />
               <SubscribeButton />
             </div>
-            <div>Voters</div>
-            <div className="py-4 flex items-center flex-col">
-              <div>Odera - RaisingCanes</div>
-              <div>Jonathan - RaisingCanes</div>
-
-              <div>James - RaisingCanes</div>
-              <div>Michael - Whataburger</div>
-              <div>Ohiyani - Whataburger</div>
+            <div className="text-5xl flex flex-col items-center lg:items-start">
+              Voters
             </div>
+            {friends ? (
+              friends.map((val, index) => {
+                return (
+                  <div
+                    key={index}
+                    className="py-4 flex items-center flex-col lg:items-start text-3xl"
+                  >
+                    {val.name} - {val.choice}
+                  </div>
+                );
+              })
+            ) : (
+              <div className="py-4 flex items-center flex-col lg:items-start text-3xl">
+                <div>Odera - RaisingCanes</div>
+                <div>Jonathan - RaisingCanes</div>
+
+                <div>James - RaisingCanes</div>
+                <div>Michael - Whataburger</div>
+                <div>Ohiyani - Whataburger</div>
+              </div>
+            )}
           </div>
         </div>
       </div>
-      <a href="https://rondevu.app/rondevu/65a589babb1e23254797ee3a">
-        CLICK ME
-      </a>
     </div>
   );
 }
