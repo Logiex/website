2522930e67902b62af17d193fb47bb5d80e108f1
Buchimi <stormzzy900@gmail.com>
2024-01-22 15:49:14 -0600

    update meta tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,11 +18,16 @@ export default function RootLayout({
       <html lang="en">
         <head>
           <title>Explore your world | Rondevu</title>
-        <meta name="apple-itunes-app" content="app-id=6474651875" />
+          <meta name="apple-itunes-app" content="app-id=6474651875" />
+          <meta name="keywords" content="Rondevu" />
+          <meta
+            name="description"
+            content="Website of the Rondevu social app"
+          />
         </head>
         <body className={`${inter.className} dark `}>
           <ApolloWrapper>{children}</ApolloWrapper>
-          <Toaster/>
+          <Toaster />
         </body>
       </html>
     </ClerkProvider>
